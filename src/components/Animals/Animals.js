36288815447fa8102f167e4a3386d96ced4ace47
AndriyYuzvenko <anbriy890@gmail.com>
2022-01-27 21:02:07 +0200

    classwork

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -1,17 +1,27 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 
-const renders = () => {
 
-}
-const cats=()=>{
 
+const renders = (animals,action) => {
+    switch (action.type){
+        case:
+
+    }
 }
 const Animals = () => {
-    const [animals, disetch] = useReducer(renders, [{cat: '', dog: ''}])
+
+    const [animals, dispatch] = useReducer(renders,{cat: '', dog: ''})
+    const save = (e) => {
+        dispatch({...animals, [e.target.name]: e.target.value})
+    }
+    const submit=(e)=>{
+        e.preventDefault()
+        console.log(animals)
+    }
     return (
         <div>
-            <form>
-                <label>Cat:<input type="text"/></label>
+            <form onSubmit={submit}>
+                <label>Cat:<input type="text" value={animals.cat} name={'cat'} onChange={save}/></label>
                 <button>Save</button>
             </form>
         </div>
