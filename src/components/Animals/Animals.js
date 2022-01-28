59b24c30495da204c24d72fc1075970b280368c3
AndriyYuzvenko <anbriy890@gmail.com>
2022-01-28 11:42:46 +0200

    classwork

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -1,34 +1,81 @@
-import React, {useReducer, useState} from 'react';
+import React, {useReducer} from 'react';
 
+const renders = (animals, action) => {
+    switch (action.type) {
+        case 'xxx':
+            return {...animals, [action.field]: action.payload}
 
-
-const renders = (animals,action) => {
-    switch (action.type){
-        case 'inc':
-            return {...animals,cat.a}
-
+        default:
+            return animals
     }
+}
+const catAndDog = {
+    cat: '', dog: ''
 }
 const Animals = () => {
 
-    const [animals, dispatch] = useReducer(renders,{cat: '', dog: ''})
-    const save = (e) => {
-        dispatch({...animals, [e.target.name]: e.target.value})
-    }
+    const [animals, dispatch] = useReducer(renders, catAndDog)
 
-    const cats=(e)=>{
+    const save = (e) => {
+        dispatch({
+            type: 'xxx',
+            field: e.target.name,
+            payload: e.target.value
+        })
+    }
+    const animalCat = (e) => {
         e.preventDefault()
         console.log(animals)
-        dispatch({type:'inc'})
+    }
+    const animalDog = (e) => {
+        e.preventDefault()
+        console.log(animals)
     }
     return (
         <div>
-            <form onSubmit={cats}>
+            <form onSubmit={animalCat}>
                 <label>Cat:<input type="text" value={animals.cat} name={'cat'} onChange={save}/></label>
                 <button>Save</button>
+            </form>2
+            <form onSubmit={animalDog}>
+                <label>Dog:<input type="text" value={animals.dog} name={'dog'} onChange={save}/></label>
+                <button>Save</button>
             </form>
+            <hr/>
+            <p>{animals.cat}</p>
+            <p>{animals.dog}</p>
         </div>
     );
 };
 
 export default Animals;
+
+// import React, {useState} from 'react';
+//
+//
+// const Animals = () => {
+//     const [cats, setCats] = useState([])
+//     const [a, setA] = useState({cat: ''})
+//     const animal = (e) => {
+//         setA({...a, [e.target.name]: e.target.value})
+//     }
+//
+//     const submit = (e) => {
+//         e.preventDefault()
+//         setCats([...cats, a])
+//
+//     }
+//     console.log(cats)
+//     return (
+//         <div>
+//             <form onSubmit={submit}>
+//                 <label>Cat:<input type="text" name={'cat'} onChange={animal}/></label>
+//                 <button>Save</button>
+//             </form>
+//             <hr/>
+//             <p>{cats.cat}</p>
+//         </div>
+//     );
+// };
+//
+// export default Animals;
